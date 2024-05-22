3ee42324bae1a084cfc60f731363e6d0ab5be5ab
twitchy-ears <45134107+twitchy-ears@users.noreply.github.com>
2024-05-22 23:37:14 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,154 @@
+<?php
+
+$data_file="theyworkforyou-mps.csv";
+$images_dir="images";
+
+# echo "file is '$data_file'<br />\n";
+
+$skipper = 0;
+$mp_data;
+foreach(file($data_file) as $line) {
+  if ($skipper == 0) {
+    $skipper += 1;
+    continue;
+  }
+
+  # "Person ID","First name","Last name",Party,Constituency,URI
+  $data = str_getcsv($line);
+
+  if ($data[3] == "Conservative") {
+    $person_id = $data[0];
+    $mp_data[$person_id] = array(
+      "person_id" => $data[0],
+      "first_name" => $data[1],
+      "last_name" => $data[2],
+      "party" => $data[3],
+      "constituency" => $data[4],
+      "twfu_url" => $data[5]
+      );
+  }
+}
+
+$mp_ids;
+$wanted_mps;
+$small_images = false;
+$freespace;
+
+if (isset($_GET["small"])) {
+  if ($_GET["small"] == "yes") {
+    $small_images = true;
+  }
+}
+
+if (isset($_GET["freespace"])) {
+  $raw = $_GET["freespace"];
+  $cooked = preg_replace('/[^[0-9]]/', '', $raw);
+  $free_space_mp = $cooked;
+}
+
+if (isset($_GET["permalink"])) {
+  $raw = $_GET["permalink"];
+  $cooked = preg_replace('/[^[0-9,]]/', '', $raw);
+  # echo "cooked: '$cooked'<br />\n";
+  $wanted_mps = explode(",", $cooked);
+}
+
+if (! isset($wanted_mps) || ! is_array($wanted_mps) || sizeof($wanted_mps) != 24) {
+  # echo "Generating random bingo<br />\n";
+  # echo "also sizeof wanted_mps: " . sizeof($wanted_mps) . "<br />";
+  # print_r($mp_data);
+  $mp_ids = array_keys($mp_data);
+  if (isset($free_space_mp)) {
+    unset($mp_ids[$free_space_mp]);
+  }
+  shuffle($mp_ids);
+  $wanted_mps = array_slice($mp_ids, 0, 24);
+}
+
+/*
+   foreach($wanted_mps as $wanted_id) {
+    if (isset($mp_data[$wanted_id]["first_name"]) && isset($mp_data[$wanted_id]["last_name"])) {
+      print "$wanted_id " . $mp_data[$wanted_id]["first_name"] . " " . $mp_data[$wanted_id]["last_name"] . "<br />\n";
+    }
+    # print "$wanted_id<br />\n";
+  }
+ */
+
+function print_mp_cell($mp_id, $pretext = "", $posttext = "") {
+  global $mp_data;
+  global $images_dir;
+  global $small_images;
+
+  $image_path="$images_dir/$mp_id.jpg";
+  $image_size = "";
+  if ($small_images) {
+    $image_size="width=\"96\" height=\"128\"";
+  }
+
+  # echo "Processing mp $mp_id<br />\n";
+  
+  if (isset($mp_data[$mp_id]["first_name"])
+      && isset($mp_data[$mp_id]["last_name"])
+      && isset($mp_data[$mp_id]["constituency"])) 
+  {
+    echo "<td>";
+    echo "<div style=\"text-align: center; margin: auto;\">";
+    echo "$pretext<img src=\"$image_path\" $image_size /><br />" . $mp_data[$mp_id]["first_name"] . " " . $mp_data[$mp_id]["last_name"] . "<br />" . $mp_data[$mp_id]["constituency"] . "$posttext</td>\n";
+  }
+  else {
+    echo "<tr>Oopsie error for MP $mp_id</tr>\n";
+  }
+}
+
+echo "<html><head><title>2024 UK Election Conservatives Losing Their Seats Bingo!</title></head><body style=\"text-align: center; margin: auto;\">";
+echo "<p>2024 UK Election Conservatives Losing Their Seats Bingo!</p>\n";
+echo "<table style=\"margin: auto;\">\n";
+echo "<tr>\n";
+print_mp_cell($wanted_mps[0]);
+print_mp_cell($wanted_mps[1]);
+print_mp_cell($wanted_mps[2]);
+print_mp_cell($wanted_mps[3]);
+print_mp_cell($wanted_mps[4]);
+echo "</tr><tr>\n";
+print_mp_cell($wanted_mps[5]);
+print_mp_cell($wanted_mps[6]);
+print_mp_cell($wanted_mps[7]);
+print_mp_cell($wanted_mps[8]);
+print_mp_cell($wanted_mps[9]);
+echo "</tr><tr>\n";
+print_mp_cell($wanted_mps[10]);
+print_mp_cell($wanted_mps[11]);
+if (isset($free_space_mp)) {
+  print_mp_cell($free_space_mp, "<b>-- FREE --</b><br/>", "<br /><b>-- SPACE --</b>");
+}
+else {
+  echo "<td style=\"text-align: center; margin: auto; font-weight: bold;\">FREE<br />SPACE</td>\n";
+}
+
+print_mp_cell($wanted_mps[12]);
+print_mp_cell($wanted_mps[13]);
+echo "</tr><tr>\n";
+print_mp_cell($wanted_mps[14]);
+print_mp_cell($wanted_mps[15]);
+print_mp_cell($wanted_mps[16]);
+print_mp_cell($wanted_mps[17]);
+print_mp_cell($wanted_mps[18]);
+echo "</tr><tr>\n";
+print_mp_cell($wanted_mps[19]);
+print_mp_cell($wanted_mps[20]);
+print_mp_cell($wanted_mps[21]);
+print_mp_cell($wanted_mps[22]);
+print_mp_cell($wanted_mps[23]);
+echo "</tr></table>\n";
+
+echo "<br /><br />";
+echo "<a href=\"index.php\">GENERATE A NEW BINGO CARD</a><br />";
+echo "<a href=\"index.php?permalink=" . join(",", $wanted_mps) . "\">PERMALINK TO THIS BINGO CARD</a><br />";
+echo "<a href=\"index.php?permalink=" . join(",", $wanted_mps) . "&freespace=25428\">PERMALINK TO THIS BINGO CARD - With Rishi Sunak as the Free Space</a><br />";
+echo "<a href=\"index.php?permalink=" . join(",", $wanted_mps) . "&small=yes\">PERMALINK TO THIS BINGO CARD - with slightly smaller images for printing on A4</a>";
+echo " - ";
+echo "<a href=\"index.php?permalink=" . join(",", $wanted_mps) . "&small=yes&freespace=25428\">small with Sunak</a>";
+
+echo "</body></html>";
+
+?>
---
+++ setup-images.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/bash
+
+url="https://www.theyworkforyou.com/mps/?f=csv"
+target="theyworkforyou-mps.csv"
+base_dir=$( pwd -P )  
+images_dir="$base_dir/images"
+image_url_base="https://www.theyworkforyou.com/people-images/mpsL/"
+image_url_fallback_base="https://www.theyworkforyou.com/people-images/mps/"
+image_url_suffix=".jpg"
+image_url_fallback_suffix=".jpeg"
+image_url_total_fail="https://www.theyworkforyou.com/images/unknownperson_large.png"
+
+if [[ ! -x $( type -P curl ) ]]; then
+  echo "No curl binary in path, failing"
+  exit 1
+fi
+
+if [[ ! -x $( type -P convert ) ]]; then
+  echo "No convert binary in path, failing"
+  exit 1
+fi
+
+if [[ ! -d "$images_dir" ]]; then
+  mkdir -p "$images_dir"
+fi
+
+if [[ ! -f "$target" ]]; then 
+  curl -L "$url" > $target
+fi
+
+mapfile -t < <( tail -n +2 "$target" )
+for line in "${MAPFILE[@]}"; do
+  # "Person ID","First name","Last name",Party,Constituency,URI
+  person_id=$( echo "$line" | cut -d, -f1 )
+  first_name=$( echo "$line" | cut -d, -f2 )
+  last_name=$( echo "$line" | cut -d, -f3 )
+  party_name=$( echo "$line" | cut -d, -f4 )
+  constituency=$( echo "$line" | cut -d, -f5 )
+  twfu_uri=$( echo "$line" | cut -d, -f6 )
+
+  # if [[ "$person_id" -ne "24934" ]]; then
+  #   echo "dull was $person_id not 24934"
+  #   continue
+  # fi
+
+  picture_path="${images_dir}/${person_id}${image_url_suffix}"
+
+  if [[ "$party_name" == "Conservative" && ! -e "$picture_path" ]]; then
+    cd "$images_dir"
+
+    picture_url="${image_url_base}${person_id}${image_url_suffix}"
+    picture_file="${person_id}${image_url_suffix}"
+    echo "Fetching: \"$picture_url\" ($first_name $last_name - $party_name)"
+    curl -L --fail-with-body "$picture_url" >> "$picture_file"
+    curl_err=$?
+
+    file_check=$( file --mime-type "$picture_file" | grep -c image/ )
+    
+    if [[ "$curl_err" -ne "0" || ! -f "$picture_file" || "$file_check" -ne "1" ]]; then
+      fallback_picture_url="${image_url_fallback_base}${person_id}${image_url_fallback_suffix}"
+      fallback_picture_file="${person_id}${image_url_fallback_suffix}"
+      echo "Failed to fetch original (curl returned '$curl_err' and '$picture_file' didn't exist or is broken) so fetching \"$fallback_picture_url\""
+      if [[ -e "$picture_file" ]]; then
+        rm $picture_file
+      fi
+      curl -L --fail-with-body "$fallback_picture_url" >> "$picture_file"
+      curl_err=$?
+      file_check=$( file --mime-type "$picture_file" | grep -c image/ )
+
+      if [[ "$curl_err" -ne "0" || ! -f "$picture_file" || "$file_check" -ne "1" ]]; then
+        echo "Failed again, downloading '$image_url_total_fail' now"
+        curl -L --fail-with-body "$image_url_total_fail" > "$picture_file.fail"
+        convert "$picture_file.fail" "$picture_file"
+        rm "$picture_file.fail"
+      fi
+    fi
+
+  fi
+
+  
+
+done
